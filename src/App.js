0cead17bcb40a46d89f6fb51720b2db5905838c6
Lucian <lucianw9977@gmail.com>
2023-06-22 11:05:32 -0700

    updated Form.js GifOptions.js App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
+import React, { useState } from 'react';
 import './App.css';
 import Form from './components/Form.js';
 import Header from './components/Header.js';
 import Footer from './components/Footer.js';
+import GifOptions from './components/GifOptions';
 
 function App() {
+  const [gifEmotion, setGifEmotion] = useState("");
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    setSearchTerm(gifEmotion);
+  };
+
   return (
     <div className="App">
       <Header/>
-      <Form/>
+      <Form 
+        gifEmotion={gifEmotion} 
+        setGifEmotion={setGifEmotion} 
+        handleSearch={handleSearch}
+      />
+      <GifOptions searchTerm={searchTerm}/>
       <Footer/>
-      
     </div>
   );
 }
+
 
 export default App;
--- src/components/GifOptions.js
+++ src/components/GifOptions.js
@@ -1,40 +1,34 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 
-function GifOptions({ emotion }) {
-  const [gif, setGif] = useState([]);
+function GifOptions({ searchTerm }) {
+  const [gifs, setGifs] = useState([]);
 
   useEffect(() => {
-    axios({
-      url: "https://api.giphy.com/v1/gifs/search",
-      method: "GET",
-      params: {
-        api_key: "1BbHy9UljgG0JLrabA8WdBhCXn8qZNuz",
-        q: emotion,
-        limit: 3,
-      },
-    }).then((res) => {
-      const gifData = res.data.data;
-      setGif(gifData);
-      console.log(res.data.data);
-    });
-  }, [emotion]);
+    if (searchTerm) {
+      axios({
+        url: "https://api.giphy.com/v1/gifs/search",
+        method: "GET",
+        params: {
+          api_key: "1BbHy9UljgG0JLrabA8WdBhCXn8qZNuz",
+          q: searchTerm,
+          limit: 3,
+        },
+      }).then((res) => {
+        setGifs(res.data.data);
+      });
+    }
+  }, [searchTerm]);
 
   return (
-    <div>
-      <p> select a gif </p>
-      <ul>
-        {gif.map((selectedGif, index) =>  (
-          <li key={index}>
-            <img src={selectedGif.images.original.url} alt="gif" />
-          </li>
-        ))}
-      </ul>
+    <div className="gifOptions">
+      {gifs.map((gif) => (
+        <img src={gif.images.fixed_height.url} alt={gif.title} key={gif.id} />
+      ))}
     </div>
-  )
+  );
 }
 
 export default GifOptions;
-
 
               
