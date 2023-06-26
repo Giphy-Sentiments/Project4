16e73cddb8ce13193f97bc638d1c50e5a0665486
Lucian <lucianw9977@gmail.com>
2023-06-26 11:50:44 -0700

    updated js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,44 @@
+// import React, { useState } from 'react';
+// import './App.css';
+// import Form from './components/Form.js';
+// import Header from './components/Header.js';
+// import Footer from './components/Footer.js';
+// import GifOptions from './components/GifOptions';
+
+// function App() {
+//   // declared two state variables to represent the emotion and search term to be used for searching gif & set value as empty string 
+
+//   // this tracks the value of the input 
+//   const [trackValue, setTrackValue] = useState("");
+  
+//   // user submitted input 
+//   const [searchTerm, setSearchTerm] = useState("");
+
+//   // count for number of times search button is clicked
+//   const [searchCount, setSearchCount] = useState(0);
+
+//   const handleSearch = (e) => {
+//     e.preventDefault();
+//     setSearchTerm(trackValue);
+//     setSearchCount((prevCount) => prevCount + 1);
+//   };
+
+//   return (
+//     <div className="App">
+//       <Header/>
+//       <Form 
+//         trackValue={trackValue} 
+//         setTrackValue={setTrackValue} 
+//         handleSearch={handleSearch}
+//       />
+//       <GifOptions searchTerm={searchTerm} searchCount={searchCount} />
+//       <Footer/>
+//     </div>
+//   );
+// }
+
+// export default App;
+
 import React, { useState } from 'react';
 import './App.css';
 import Form from './components/Form.js';
@@ -6,21 +47,15 @@ import Footer from './components/Footer.js';
 import GifOptions from './components/GifOptions';
 
 function App() {
-  // declared two state variables to represent the emotion and search term to be used for searching gif & set value as empty string 
-
-  // this tracks the value of the input 
   const [trackValue, setTrackValue] = useState("");
-  
-  // user submitted input 
   const [searchTerm, setSearchTerm] = useState("");
-
-  // count for number of times search button is clicked
   const [searchCount, setSearchCount] = useState(0);
 
   const handleSearch = (e) => {
     e.preventDefault();
     setSearchTerm(trackValue);
     setSearchCount((prevCount) => prevCount + 1);
+    setTrackValue("");
   };
 
   return (
--- src/components/GifOptions.js
+++ src/components/GifOptions.js
@@ -1,96 +1,123 @@
 // import React, { useEffect, useState } from "react";
 // import axios from "axios";
 
-// function GifOptions({ searchTerm }) {
+// function GifOptions({ searchTerm, searchCount }) {
 //   const [gifs, setGifs] = useState([]);
+
+//   const fetchRandomGif = () => {
+//     axios({
+//       url: "https://api.giphy.com/v1/gifs/random",
+//       method: "GET",
+//       params: {
+//         api_key: "1BbHy9UljgG0JLrabA8WdBhCXn8qZNuz",
+//         tag: searchTerm,
+//       },
+//     }).then((res) => {
+//       setGifs((oldGifs) => [...oldGifs, res.data.data]);
+//     });
+//   };
 
 //   useEffect(() => {
 //     if (searchTerm) {
-//       axios({
-//         url: "https://api.giphy.com/v1/gifs/search",
-//         method: "GET",
-//         params: {
-//           api_key: "1BbHy9UljgG0JLrabA8WdBhCXn8qZNuz",
-//           q: searchTerm,
-//           limit: 3,
-//           // random_id:""
-//         },
-//       }).then((res) => {
-//         console.log(res.data.data)
-//         setGifs(res.data.data);
-//       });
+//       setGifs([]);
+//       for (let i = 0; i < 3; i++) {
+//         fetchRandomGif();
+//       }
 //     }
-//   }, [searchTerm]);
+//   }, [searchTerm, searchCount]);
+
+//   const handleMoreGifs = () => {
+//     setGifs([]);
+//     for (let i = 0; i < 3; i++) {
+//       fetchRandomGif();
+//     }
+//   };
 
 //   return (
 //     <ul className="gifOptions">
 //       <li className="gifList">
-//       {gifs.map((gif) => (
-//         <img src={gif.images.original.url} 
-//         alt={gif.title} 
-//         key={gif.id} 
-//         style={{width:'200px', height: '200px'}}/>
-//       ))}
+//         {gifs.map((gif) => (
+//           <img
+//             src={gif.images.original.url}
+//             alt={gif.title}
+//             key={gif.id}
+//             style={{ width: "200px", height: "200px" }}
+//           />
+//         ))}
 //       </li>
-//       <button>More gifs</button>
+//       <button onClick={handleMoreGifs}>More gifs</button>
 //     </ul>
 //   );
 // }
 
 // export default GifOptions;
 
-
-              
-
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 function GifOptions({ searchTerm, searchCount }) {
-  const [gifs, setGifs] = useState([]);
+  const [gifs, setGifs] = useState([[]]);
+  const [offset, setOffset] = useState(0);
+  const [alert, setAlert] = useState(false);
 
-  const fetchRandomGif = () => {
+  const fetchGifs = () => {
     axios({
-      url: "https://api.giphy.com/v1/gifs/random",
+      url: "https://api.giphy.com/v1/gifs/search",
       method: "GET",
       params: {
         api_key: "1BbHy9UljgG0JLrabA8WdBhCXn8qZNuz",
-        tag: searchTerm,
+        q: searchTerm,
+        limit: 3,
+        offset: offset,
       },
     }).then((res) => {
-      setGifs((oldGifs) => [...oldGifs, res.data.data]);
+      setGifs((oldGifs) => {
+        const newGifs = [...oldGifs];
+        newGifs[newGifs.length - 1] = res.data.data;
+        return newGifs;
+      });
+      setOffset((prevOffset) => prevOffset + 3);
     });
   };
 
   useEffect(() => {
     if (searchTerm) {
-      setGifs([]);
-      for (let i = 0; i < 3; i++) {
-        fetchRandomGif();
-      }
+      setGifs([[]]);
+      setOffset(0);
+      setAlert(false);
+      fetchGifs();
     }
   }, [searchTerm, searchCount]);
 
+
   const handleMoreGifs = () => {
-    setGifs([]);
-    for (let i = 0; i < 3; i++) {
-      fetchRandomGif();
+    if (gifs.length < 3) {
+      setGifs((oldGifs) => [...oldGifs, []]);
+      fetchGifs();
+    } else {
+      setAlert(true);
     }
   };
 
+
   return (
-    <ul className="gifOptions">
-      <li className="gifList">
-        {gifs.map((gif) => (
-          <img
-            src={gif.images.original.url}
-            alt={gif.title}
-            key={gif.id}
-            style={{ width: "200px", height: "200px" }}
-          />
-        ))}
-      </li>
+    <div className="gifOptions">
+      {gifs.map((gifRow, index) => (
+        <ul className="gifList" key={index}>
+          {gifRow.map((gif) => (
+            <li key={gif.id}>
+              <img
+                src={gif.images.original.url}
+                alt={gif.title}
+                style={{ width: "200px", height: "200px" }}
+              />
+            </li>
+          ))}
+        </ul>
+      ))}
+      {alert && <div>You can only search for a total of 9 gifs</div>}
       <button onClick={handleMoreGifs}>More gifs</button>
-    </ul>
+    </div>
   );
 }
 
