ef83de553745008d52b3d640fff064161b21c793
Jay Park <109562546+JihyeJayPark@users.noreply.github.com>
2023-07-06 22:09:31 -0400

    Revert "Jay test"

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import React, { useState } from "react";
-import "./App.css";
-import Form from "./components/Form.js";
-import Header from "./components/Header.js";
-import Footer from "./components/Footer.js";
-import GifOptions from "./components/GifOptions";
-import Timeline from "./components/Timeline";
-import { Link, Route, Routes } from "react-router-dom";
-import Home from "./components/Home.js"
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />}/>
-        <Route path="/timeline" element={<Timeline />}/>
-      </Routes>
-      
-      <Footer />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
